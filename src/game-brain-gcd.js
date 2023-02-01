651ece71c63210ec6695cd7521cff8a9ee909231
Mari Kruk <mari.krukovskaya.97@mail.ru>
2023-02-01 16:19:33 +0300

    finish game brain-gcd

---
+++ src/game-brain-gcd.js
@@ -0,0 +1,33 @@
+import readlineSync from 'readline-sync';
+import userName from './cli.js';
+
+const randomNumber = () => Math.floor(Math.random() * 50);
+
+const nod = (a, b) => {
+  while (a !== b ) {
+    if(a > b) {
+      a = a - b;
+    } else {
+      b = b - a;
+    }
+  }
+  return a;
+}
+
+export default function brainGcd() {
+ const name = userName();
+console.log('Find the greatest common divisor of given numbers.');
+ for(let i = 0; i < 3; i += 1) {
+ const num1 = randomNumber();
+ const num2 = randomNumber(); 
+ const questionNODA = readlineSync.question((`${'Question: '}${num1} ${num2} ${'\nYour answer: '}`));
+ const noda = nod(num1, num2);
+ if(noda === Number(questionNODA)) {
+    console.log('Correct!');
+ } else {
+    console.log(`'${questionNODA}' is wrong answer ;( Correct answer was '${noda}'. \nLet's try again, ${name}`);
+    return;
+  }
+ }
+ console.log(`Congratulations, ${name}`);
+}

4fca97d0bf713a7790e3ebc0b598ca54c3a14efb
Mari Kruk <mari.krukovskaya.97@mail.ru>
2023-02-06 19:28:38 +0300

    error correction games brain-progression

--- src/game-brain-prog.js
+++ src/game-brain-prog.js
@@ -1,37 +1,28 @@
-
-import readlineSync from 'readline-sync';
-import userName from './cli.js';
+import startTheGames from './index.js';
 
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
- 
- const progression = () => {
- const progressionLength = randomNumber(10, 20);
- const progressionNumber = randomNumber(1, 20);
- const count = randomNumber(1, 5);
- const num = [progressionNumber];
-for (let i = 0; num.length <= progressionLength; i += 1) {
- num.push(num[i] + count);
- }
-  return num;
+const rules = 'What number is missing in the progression?';
+
+const brainGames = () => {
+  const progression = () => {
+    const progressionLength = randomNumber(10, 20);
+    const progressionNumber = randomNumber(1, 20);
+    const count = randomNumber(2, 5);
+    const num = [progressionNumber];
+    for (let i = 0; num.length <= progressionLength; i += 1) {
+      num.push(num[i] + count);
+    }
+    return num;
+  };
+  const funcProgression = progression();
+  const numberRandom = randomNumber(1, 20);
+  const result = randomNumber(0, numberRandom);
+  const ancwer = funcProgression[result];
+  funcProgression[result] = '..';
+  const progArifmetica = funcProgression.join(' ');
+  const question = `${progArifmetica}`;
+  return [question, ancwer];
 };
-export default function brainProg() {
- const name = userName();
- console.log(readlineSync.question('What number is missing in the progression?'));
- for (let i = 0; i < 3; i += 1) {
- const stack = progression();
- const numberRandom = randomNumber(0, 20);
- const result = randomNumber(numberRandom, 1);
- const prog = String(stack[result]);
- stack[result] = '..';
- const progArifmetica = stack.join(' '); 
- const questionProgression = readlineSync.question((`${'Question: '}${progArifmetica}${'\nYour answer: '}`));
- if (prog === questionProgression) {
-console.log('Correct!');
-} else {
-console.log(`'${questionProgression}' is wrong answer ;( Correct answer was '${prog}'. \nLet's try again, ${name}`);
- return;
- }
- 
-}
-console.log(`Congratulations, ${name}`);
-}
+export default () => {
+  startTheGames(rules, brainGames);
+};

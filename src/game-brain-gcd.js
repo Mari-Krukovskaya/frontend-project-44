5f17a638c85c7aeb48fc5b3e3f34f2687bf4730b
Mari Kruk <mari.krukovskaya.97@mail.ru>
2023-02-06 19:27:19 +0300

    error correction games brain-gcd

--- src/game-brain-gcd.js
+++ src/game-brain-gcd.js
@@ -1,34 +1,27 @@
-import readlineSync from 'readline-sync';
-import userName from './cli.js';
+import startTheGames from './index.js';
 
 const randomNumber = () => Math.floor(Math.random() * 50);
+const rules = 'Find the greatest common divisor of given numbers.';
 
-const nod = (a, b) => {
-  while (a !== b ) {
- if (a > b) {
-  a -= b;
-} else {
-  b -= a;
-  }
-}
-  return a;
-}
-
-export default function brainGcd() {
- const name = userName();
-console.log('Find the greatest common divisor of given numbers.');
- for(let i = 0; i < 3; i += 1) {
- const num1 = randomNumber();
- const num2 = randomNumber(); 
- const questionNODA = readlineSync.question((`${'Question: '}${num1} ${num2} ${'\nYour answer: '}`));
- const noda = nod(num1, num2);
- if(noda === Number(questionNODA)) {
-    console.log('Correct!');
- } else {
-    console.log(`'${questionNODA}' is wrong answer ;( Correct answer was '${noda}'. \nLet's try again, ${name}`);
-    return;
-  }
- }
- console.log(`Congratulations, ${name}`);
- 
-}
+const brainGames = () => {
+  const nod = (a, b) => {
+    let nod1 = a;
+    let nod2 = b;
+    while (nod1 !== nod2) {
+      if (nod1 > nod2) {
+        nod1 -= nod2;
+      } else {
+        nod2 -= nod1;
+      }
+    }
+    return nod1;
+  };
+  const num1 = randomNumber();
+  const num2 = randomNumber();
+  const question = `${num1} ${num2}`;
+  const ancwer = nod(num1, num2);
+  return [question, ancwer];
+};
+export default () => {
+  startTheGames(rules, brainGames);
+};

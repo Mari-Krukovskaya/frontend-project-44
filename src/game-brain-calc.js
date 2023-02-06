2ae0d6e04ed659b0e66637c12257c716bde21c83
Mari Kruk <mari.krukovskaya.97@mail.ru>
2023-02-06 19:24:56 +0300

    error correction brain-calc

--- src/game-brain-calc.js
+++ src/game-brain-calc.js
@@ -1,42 +1,35 @@
-import readlineSync from 'readline-sync';
-import userName from './cli.js';
+import startTheGames from './index.js';
 
 const randomNumber = () => Math.round(Math.random() * 10);
+const rules = 'What is the result of the expression?';
 
-const randomOperator = () => {
-  const operator = ['+', '-', '*'];
-  const operatorCalc = Math.floor(Math.random() * operator.length);
-  const result = operator[operatorCalc];
-  return result;
-};
-const calc = (num1, num2, operator) => {
-  switch (operator) {
+const brainGames = () => {
+  const randomOperator = () => {
+    const operator = ['+', '-', '*'];
+    const operatorCalc = Math.floor(Math.random() * operator.length);
+    const result = operator[operatorCalc];
+    return result;
+  };
+  const calc = (num1, num2, operator) => {
+    switch (operator) {
       case '+':
-   return num2 + num1;
+        return num2 + num1;
       case '-':
-   return num2 - num1;
+        return num2 - num1;
       case '*':
-   return num2 * num1;
+        return num2 * num1;
       default:
-    break;
-  } 
-  return operator;
-};
-export default function brainCalc() {
-  const name = userName();
-  console.log(readlineSync.question('What is the result of the expression?'));
-  for (let i = 0; i < 3; i += 1) {
+        break;
+    }
+    return operator;
+  };
   const num1 = randomNumber();
   const num2 = randomNumber();
-  const operator = randomOperator()
-  const questionCalc = readlineSync.question((`${'Question: '}${num2} ${operator} ${num1} ${'\nYour answer: '}`));
-  const funcCalc = calc(num1, num2, operator);
-  if (funcCalc === Number(questionCalc)) {
-  console.log('Correct!');
-} else {
-  console.log(`'${questionCalc}' is wrong answer ;( Correct answer was '${funcCalc}'. \nLet's try again, ${name}`);
-   return;
-  }
- }
- console.log(`Congratulations, ${name}`);
-}
+  const operator = randomOperator();
+  const question = `${num2} ${operator} ${num1}`;
+  const ancwer = calc(num1, num2, operator);
+  return [question, ancwer];
+};
+export default () => {
+  startTheGames(rules, brainGames);
+};

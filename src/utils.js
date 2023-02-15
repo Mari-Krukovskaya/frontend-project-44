070801665b42bc9a8f195c7453775b9e5840d732
Mari Kruk <mari.krukovskaya.97@mail.ru>
2023-02-15 12:43:26 +0300

    correction errors

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,7 @@
 
-export default (max = 100, min = 0) => Math.floor(Math.random() * (max - min) + min);
-
-
+export default (max = 100, min = 0) => Math.floor(Math.random() * (max - min + 1) + min);
+// export default(min = 0, max = 100) => {
+//  const minRandom = Math.ceil(min);
+//  const maxRandom = Math.floor(max);
+//   return Math.floor(Math.random() * (maxRandom - minRandom + 1)) + minRandom; //Максимум и минимум включаются
+//}
